05b90330522673a42031869311410f6699c1c311
Ruthvik Garlapati <garlapati.r@northeastern.edu>
2024-03-25 01:36:04 -0400

    Intial Code Added

---
+++ CanvasClone/src/main/java/com/oodproject/canvasclone/CanvasClone.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.oodproject.canvasclone;
+
+import uipackage.MainJFrame;
+
+/**
+ *
+ * @author ruthvikg
+ */
+public class CanvasClone {
+
+    public static void main(String[] args) {
+        new MainJFrame().setVisible(true);
+        
+    }
+}
